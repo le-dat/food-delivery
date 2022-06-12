65fe6a6ca02649accbdd8bfb147930d4e431ab54
le-dat <lqdat87@gmail.com>
2022-06-12 10:09:06 +0700

    js change, remove avatar

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -8,3 +8,30 @@ settingItems.forEach((item) => {
     item.classList.add("active");
   });
 });
+
+const inputFile = $("#input__file");
+const btnChangeAvatar = $(".setting__btn.btn__change");
+const btnRemoveAvatar = $(".setting__btn.btn__remove");
+const avatar = $(".setting__avatarImg");
+
+btnChangeAvatar.addEventListener("click", () => {
+  inputFile.click();
+});
+btnRemoveAvatar.addEventListener("click", () => {
+  if (avatar) {
+    URL.revokeObjectURL(avatar.view);
+    avatar.setAttribute(
+      "src",
+      "https://i.pinimg.com/564x/d9/7b/bb/d97bbb08017ac2309307f0822e63d082.jpg"
+    );
+  }
+});
+
+inputFile.onchange = (e) => {
+  if (avatar) {
+    URL.revokeObjectURL(avatar.view);
+  }
+  const file = e.target.files[0];
+  avatar.view = URL.createObjectURL(file);
+  avatar.setAttribute("src", avatar.view);
+};
